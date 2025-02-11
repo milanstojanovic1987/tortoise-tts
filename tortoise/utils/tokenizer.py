665ddc487cc4bad1a070a156344681ca2a81fc83
milans213 <110895013+milanstojanovic1987@users.noreply.github.com>
2025-02-11 17:08:39 +0100

    Update tokenizer.py

--- tortoise/utils/tokenizer.py
+++ tortoise/utils/tokenizer.py
@@ -141,10 +141,10 @@ def transliteration_cleaners(text):
 
 def english_cleaners(text):
   '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
+  # text = convert_to_ascii(text)
   text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
+  # text = expand_numbers(text)
+  # text = expand_abbreviations(text)
   text = collapse_whitespace(text)
   text = text.replace('"', '')
   return text
@@ -166,7 +166,7 @@ def lev_distance(s1, s2):
   return distances[-1]
 
 
-DEFAULT_VOCAB_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/tokenizer.json')
+DEFAULT_VOCAB_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/custom_language_tokenizer.json')
 
 
 class VoiceBpeTokenizer:
