e439df17dec26e8635c0eebab77cd4b6d56ecd88
milans213 <110895013+milanstojanovic1987@users.noreply.github.com>
2025-02-12 15:19:14 +0100

    Update autoregressive.py

--- tortoise/models/autoregressive.py
+++ tortoise/models/autoregressive.py
@@ -76,26 +76,27 @@ class GPT2InferenceModel(GPT2PreTrainedModel):
         self.cached_mel_emb = mel_emb
 
     def prepare_inputs_for_generation(self, input_ids, past_key_values=None, **kwargs):
-        token_type_ids = kwargs.get("token_type_ids", None)  # usually None
+        token_type_ids = kwargs.get("token_type_ids", None)
         if not self.kv_cache:
             past_key_values = None
-        # only last token for inputs_ids if past is defined in kwargs
         if past_key_values:
             input_ids = input_ids[:, -1].unsqueeze(-1)
             if token_type_ids is not None:
                 token_type_ids = token_type_ids[:, -1].unsqueeze(-1)
-
+        
         attention_mask = kwargs.get("attention_mask", None)
+        if attention_mask is None:
+            attention_mask = torch.ones_like(input_ids)
+        
         position_ids = kwargs.get("position_ids", None)
-
         if attention_mask is not None and position_ids is None:
-            # create position_ids on the fly for batch generation
             position_ids = attention_mask.long().cumsum(-1) - 1
             position_ids.masked_fill_(attention_mask == 0, 1)
             if past_key_values:
                 position_ids = position_ids[:, -1].unsqueeze(-1)
         else:
             position_ids = None
+    
         return {
             "input_ids": input_ids,
             "past_key_values": past_key_values,
@@ -104,6 +105,7 @@ class GPT2InferenceModel(GPT2PreTrainedModel):
             "attention_mask": attention_mask,
             "token_type_ids": token_type_ids,
         }
+
 
     def forward(
         self,
